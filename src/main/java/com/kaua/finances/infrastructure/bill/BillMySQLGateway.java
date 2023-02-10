a4d722f966b072c9ef189b7b9cfef4d4fde3b9cf
Kauã Pereira <zkauacodes@hotmail.com>
2023-02-10 14:51:31 -0300

    feat: add BillGateway implementation to BillMySQLGateway

---
+++ src/main/java/com/kaua/finances/infrastructure/bill/BillMySQLGateway.java
@@ -0,0 +1,41 @@
+package com.kaua.finances.infrastructure.bill;
+
+import com.kaua.finances.domain.bills.Bill;
+import com.kaua.finances.domain.bills.BillGateway;
+import com.kaua.finances.infrastructure.bill.persistence.BillJpaFactory;
+import com.kaua.finances.infrastructure.bill.persistence.BillRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+import java.util.Optional;
+
+@Component
+public class BillMySQLGateway implements BillGateway {
+
+    private final BillRepository billRepository;
+
+    public BillMySQLGateway(final BillRepository billRepository) {
+        this.billRepository = Objects.requireNonNull(billRepository);
+    }
+
+    @Override
+    public Bill create(Bill aBill) {
+        final var entity = this.billRepository.save(BillJpaFactory.from(aBill));
+        return BillJpaFactory.toDomain(entity);
+    }
+
+    @Override
+    public void deleteById(String id) {
+
+    }
+
+    @Override
+    public Optional<Bill> findById(String id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Bill update(Bill aBill) {
+        return null;
+    }
+}
