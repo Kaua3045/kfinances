cda381ea3b9c90c839400709bdd77dd3345bec53
Kauã Pereira <zkauacodes@hotmail.com>
2023-02-11 11:00:03 -0300

    feat: add DeleteBillUseCase, implementation and tests

---
+++ src/main/java/com/kaua/finances/application/usecases/bill/DefaultDeleteBillByIdUseCase.java
@@ -0,0 +1,20 @@
+package com.kaua.finances.application.usecases.bill;
+
+import com.kaua.finances.domain.bills.BillGateway;
+
+import java.util.Objects;
+
+public class DefaultDeleteBillByIdUseCase implements DeleteBillByIdUseCase {
+
+    private final BillGateway billGateway;
+
+    public DefaultDeleteBillByIdUseCase(final BillGateway billGateway) {
+        this.billGateway = Objects.requireNonNull(billGateway);
+    }
+
+
+    @Override
+    public void execute(String id) {
+        this.billGateway.deleteById(id);
+    }
+}
---
+++ src/main/java/com/kaua/finances/application/usecases/bill/DeleteBillByIdUseCase.java
@@ -0,0 +1,6 @@
+package com.kaua.finances.application.usecases.bill;
+
+public interface DeleteBillByIdUseCase {
+
+    void execute(String id);
+}
