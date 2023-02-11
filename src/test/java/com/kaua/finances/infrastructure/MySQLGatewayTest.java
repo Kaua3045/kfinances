3c86a02d636cdc1b27a0644d87501955502b13ee
Kauã Pereira <zkauacodes@hotmail.com>
2023-02-11 18:12:32 -0300

    fix: add useDefaultFilters to disable filters

--- src/test/java/com/kaua/finances/infrastructure/MySQLGatewayTest.java
+++ src/test/java/com/kaua/finances/infrastructure/MySQLGatewayTest.java
@@ -16,8 +16,10 @@ import java.lang.annotation.*;
 @DataJpaTest
 @ComponentScan(
         basePackages = "com.kaua.finances",
+        // fez os testes funcionar, tirou os filtros do security
+        useDefaultFilters = false,
         includeFilters = {
-                @ComponentScan.Filter(type = FilterType.REGEX, pattern = ".[MySQLGateway]")
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = ".*MySQLGateway")
         }
 )
 @ExtendWith(MySQLCleanUpExtension.class)
--- src/test/java/com/kaua/finances/infrastructure/account/api/AccountAPITest.java
+++ src/test/java/com/kaua/finances/infrastructure/account/api/AccountAPITest.java
@@ -27,7 +27,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
-import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.Matchers.*;
