b2cd8cbf53a175ca274711610a31f7898e513919
Kauã Pereira <zkauacodes@hotmail.com>
2023-02-11 18:03:26 -0300

    chore: add Slf4j to logs

--- src/main/java/com/kaua/finances/infrastructure/configuration/security/WebSecurityConfiguration.java
+++ src/main/java/com/kaua/finances/infrastructure/configuration/security/WebSecurityConfiguration.java
@@ -1,22 +1,86 @@
 package com.kaua.finances.infrastructure.configuration.security;
 
+import com.kaua.finances.domain.account.AccountGateway;
+import com.kaua.finances.infrastructure.security.jwt.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.ArrayList;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration {
 
+    private final AccountGateway accountGateway;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public WebSecurityConfiguration(AccountGateway accountGateway, JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.accountGateway = accountGateway;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
     @Bean
-    public SecurityFilterChain configure(final HttpSecurity http) throws Exception {
+    public SecurityFilterChain configure(
+            HttpSecurity http
+    ) throws Exception {
         return http
                 .csrf()
                 .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/auth", "/accounts", "/error")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> {
+            final var aAccount = accountGateway.findByEmail(username);
+
+            return new User(
+                    aAccount.get().getEmail(),
+                    aAccount.get().getPassword(),
+                    true,
+                    true,
+                    true,
+                    true,
+                    new ArrayList<>()
+            );
+        };
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(bCryptPasswordEncoder());
+
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
     @Bean
