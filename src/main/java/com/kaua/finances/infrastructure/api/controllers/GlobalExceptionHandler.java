a18e637d2f347470d26264d282edddef6ac24290
Kauã Pereira <zkauacodes@hotmail.com>
2023-02-14 09:28:37 -0300

    feat: add InternalServerExceptionHandling in GlobalExceptionHandler

--- src/main/java/com/kaua/finances/infrastructure/api/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/kaua/finances/infrastructure/api/controllers/GlobalExceptionHandler.java
@@ -3,19 +3,25 @@ package com.kaua.finances.infrastructure.api.controllers;
 import com.kaua.finances.application.exceptions.DomainException;
 import com.kaua.finances.application.exceptions.NotFoundException;
 import com.kaua.finances.infrastructure.utils.ApiError;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Collections;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(AccessDeniedException.class)
     public ResponseEntity<?> handleAccessDeniedException(final AccessDeniedException ex) {
         return ResponseEntity.status(HttpStatus.FORBIDDEN)
-                .body(new ApiError(null, null));
+                .body(new ApiError(ex.getMessage(), null));
     }
 
     @ExceptionHandler(NotFoundException.class)
@@ -28,5 +34,12 @@ public class GlobalExceptionHandler {
     public ResponseEntity<?> handleDomainException(final DomainException ex) {
         return ResponseEntity.unprocessableEntity()
                 .body(new ApiError(ex.getMessage(), ex.getErrors()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleInternalServerError(final Exception ex) {
+        logger.error("Internal server error: ", ex);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ApiError("Erro inesperado", Collections.emptyList()));
     }
 }
