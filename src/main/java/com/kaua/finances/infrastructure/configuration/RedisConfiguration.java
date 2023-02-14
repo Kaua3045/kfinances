baf89df6dce30504573eb26f1fba97b3ee28e804
Kauã Pereira <zkauacodes@hotmail.com>
2023-02-14 13:55:35 -0300

    feat: add redis template bean config

--- src/main/java/com/kaua/finances/infrastructure/configuration/RedisConfiguration.java
+++ src/main/java/com/kaua/finances/infrastructure/configuration/RedisConfiguration.java
@@ -5,8 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
+@EnableRedisRepositories
 public class RedisConfiguration {
 
     @Value("${spring.redis.host}")
@@ -22,5 +25,12 @@ public class RedisConfiguration {
                 redisHost,
                 Integer.parseInt(redisPort)
         ));
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
     }
 }
