7a64c5439545d4e7eb9c131c1899f10c2e80aad6
Kauã Pereira <zkauacodes@hotmail.com>
2023-02-14 13:58:01 -0300

    feat: add AccountRedisGateway in get account usecase

--- src/main/java/com/kaua/finances/application/usecases/account/retrieve/DefaultGetAccountByIdUseCase.java
+++ src/main/java/com/kaua/finances/application/usecases/account/retrieve/DefaultGetAccountByIdUseCase.java
@@ -4,6 +4,7 @@ import com.kaua.finances.application.exceptions.NotFoundException;
 import com.kaua.finances.domain.account.Account;
 import com.kaua.finances.domain.account.AccountGateway;
 import com.kaua.finances.application.usecases.account.output.AccountOutput;
+import com.kaua.finances.domain.account.AccountRedisGateway;
 
 import java.util.Objects;
 import java.util.function.Supplier;
@@ -11,16 +12,17 @@ import java.util.function.Supplier;
 public class DefaultGetAccountByIdUseCase implements GetAccountByIdUseCase {
 
     private final AccountGateway accountGateway;
+    private final AccountRedisGateway accountRedisGateway;
 
-    public DefaultGetAccountByIdUseCase(final AccountGateway accountGateway) {
+    public DefaultGetAccountByIdUseCase(final AccountGateway accountGateway, final AccountRedisGateway accountRedisGateway) {
         this.accountGateway = Objects.requireNonNull(accountGateway);
+        this.accountRedisGateway = Objects.requireNonNull(accountRedisGateway);
     }
 
     @Override
     public AccountOutput execute(String id) {
-        final var aAccount = this.accountGateway.findById(id)
-                .orElseThrow(notFound(id));
-
+        final var aAccount = this.accountRedisGateway.findById(id)
+                .orElseGet(() -> this.accountGateway.findById(id).orElseThrow(notFound(id)));
 
         return AccountOutput.from(aAccount);
     }
