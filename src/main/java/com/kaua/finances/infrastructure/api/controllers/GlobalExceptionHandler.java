89f2b9595010fc388a019869b278c2eed1fe9c0b
Kauã Pereira <zkauacodes@hotmail.com>
2023-02-15 18:57:48 -0300

    fix: add Bad Credentials exception handling

--- src/main/java/com/kaua/finances/infrastructure/api/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/kaua/finances/infrastructure/api/controllers/GlobalExceptionHandler.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -18,6 +19,12 @@ import java.util.Collections;
 public class GlobalExceptionHandler {
 
     private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<?> handleBadCredentialsException(final BadCredentialsException ex) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                .body(new ApiError(ex.getMessage(), null));
+    }
 
     @ExceptionHandler(ExpiredJwtException.class)
     public ResponseEntity<?> handleExpiredJwtException(final ExpiredJwtException ex) {
